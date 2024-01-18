4d5a51178c235f74d939cd8f339479460222d706
RathinaKumar-07 <85558773+RathinaKumar-07@users.noreply.github.com>
2024-01-18 19:01:01 +0530

    Update imageRoute.js

--- src/image/imageRoute.js
+++ src/image/imageRoute.js
@@ -2,30 +2,41 @@ const { upload } = require("../database/imageMulter")
 
 const express = require("express");
 
-const router =express.Router()
+const router = express.Router()
 
-router.post('/upload', upload.array('photos',10),(req,res)=>{
-    // console.log("req",req.files)
-    // console.log('re',req.body.time)
-    const data = [...req.files];
-    const time = req.body.time;
+router.post('/upload', upload.array('photos', 10), async (req, res) => {
 
-    const dateString = new Date(time * 1000).toLocaleString();
+    try {
+        const data = [...req.files];
+        const times = Array.isArray(req.body.time) ? req.body.time : [req.body.time];
 
-    
-    if (req.files) {
-        const updatedData = data.map(file => {
-            return {
-                ...file,
-                "StartTime": dateString
-            };
-        });
-    
-        res.json({
-            message: "Image Uploaded",
-            data: updatedData
-        });
+        const dateStringArray = times.map(time => new Date(Number(time)).toLocaleString("en-US", {
+            timeZone: 'Asia/Kolkata' // Specify UTC for consistency
+        }));
+
+        if (req.files) {
+            const updatedData = data.map((file, index) => {
+                return {
+                    ...file,
+                    "time": dateStringArray[index]
+                };
+            });
+            // console.log(updatedData)
+
+            res.json({
+                status: 200,
+                message: "Image Uploaded",
+                data: updatedData
+            });
+        }
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            success: 0,
+            message: "Image Upload Error!"
+        })
     }
 });
 
-module.exports =router
+module.exports = router
